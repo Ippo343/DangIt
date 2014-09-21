315d91ace1b0da4d7c2bf6b92f9366358653aee0
Michele <m.ippolito@outlook.com>
2014-09-21 21:57:54 +0200

    Added filtering options in the crew window
    The crew window now shows toggles to filter the list of kerbals.
    Changed the perk requirements for engines and RCS.

--- source/DangIt/Runtime/GUI/RosterWindow.cs
+++ source/DangIt/Runtime/GUI/RosterWindow.cs
@@ -9,11 +9,27 @@ namespace ippo
 {
     class RosterWindow
     {
+        delegate bool RosterFilter(ProtoCrewMember k);
+
         int selectionIdx = 0;
-        Rect rosterRect = new Rect(300, 100, 400, 200);
+        Rect rosterRect = new Rect(300, 100, 600, 300);
         Vector2 scrollPos = new Vector2(0, 0);
 
+        bool showCrew;
+        bool showAssigned;
+        bool showHired;
+        bool showApplicants;
+
         public bool Enabled { get; set; }
+
+
+        public RosterWindow()
+        {
+            showCrew = HighLogic.LoadedSceneIsFlight;
+            showAssigned = !HighLogic.LoadedSceneIsFlight;
+            showHired = !HighLogic.LoadedSceneIsFlight;
+            showApplicants = !HighLogic.LoadedSceneIsFlight;
+        }
 
 
         public void Draw()
@@ -33,20 +49,53 @@ namespace ippo
 
             GUILayout.BeginHorizontal();
 
-            #region Left side: list of kerbals
-            
+            #region Filters
+            GUILayout.BeginVertical();
+             
+            // If no filter is selected, apply a default filter
+            if (!(this.showCrew || this.showAssigned || this.showHired || this.showApplicants))
+            {
+                if (HighLogic.LoadedSceneIsFlight)
+                    showCrew = true;    // in flight, default filter is crew
+                else
+                    showHired = true;   // outside, default filter is hired
+            }
+
+            // Create the toggles to select the filter
+            showCrew = (HighLogic.LoadedSceneIsFlight) ? GUILayout.Toggle(showCrew, "Crew") : false; // crew is not available when not in flight
+            showAssigned = GUILayout.Toggle(showAssigned, "Assigned");
+            showHired = GUILayout.Toggle(showHired, "Hired");
+            showApplicants = GUILayout.Toggle(showApplicants, "Applicants");            
+
+            // Filter function that selects the kerbals from the roster based on the user's selection
+            RosterFilter filter = 
+                k => (showCrew && (FlightGlobals.ActiveVessel != null) ? FlightGlobals.ActiveVessel.GetVesselCrew().Contains(k) : false)
+                  || (showAssigned && k.rosterStatus == ProtoCrewMember.RosterStatus.Assigned)
+                  || (showHired && HighLogic.CurrentGame.CrewRoster.Crew.Contains(k))
+                  || (showApplicants && HighLogic.CurrentGame.CrewRoster.Applicants.Contains(k));
+
+            GUILayout.EndVertical();
+
+            #endregion
+
+            #region List of kerbals
+
             scrollPos = GUILayout.BeginScrollView(scrollPos, GUIStyle.none);
 
-            // In flight only show assigned crew
-            ProtoCrewMember[] crew = HighLogic.LoadedSceneIsFlight ? 
-                HighLogic.CurrentGame.CrewRoster.Crew.Where(k => k.rosterStatus == ProtoCrewMember.RosterStatus.Assigned).ToArray() :
-                HighLogic.CurrentGame.CrewRoster.Crew.ToArray();
+            // Join the crew and applicants rosters into one
+            var allKerbals = HighLogic.CurrentGame.CrewRoster.Applicants.Concat(
+                             HighLogic.CurrentGame.CrewRoster.Crew);
 
-            selectionIdx = GUILayout.SelectionGrid(selectionIdx,
-                                                   crew.Select(k => k.name).ToArray(),
-                                                   xCount: 1);
+            // Filter them using the closure created above
+            ProtoCrewMember[] selectedKerbals = allKerbals.Where(k => filter(k)).ToArray();
 
-            kerbalName = crew.ElementAt(selectionIdx).name;
+            if (selectedKerbals.Count() > 0)
+            {
+                selectionIdx = GUILayout.SelectionGrid(selectionIdx,
+                                                           selectedKerbals.Select(k => k.name).ToArray(),
+                                                           xCount: 1);
+                kerbalName = selectedKerbals.ElementAt(selectionIdx).name; 
+            }
 
             GUILayout.EndScrollView(); 
 
@@ -54,6 +103,7 @@ namespace ippo
 
             #region Right side: kerbal data
 
+            // Fetch the data from CrewFiles
             string text = "No information available";
             if (CrewFilesManager.CrewFilesInstalled &&
                 CrewFilesManager.Server != null &&
