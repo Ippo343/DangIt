5b30b30c53a4ea764713fe41f0d4928327b4d52f
Louis Goessling <louis@goessling.com>
2014-12-09 11:45:56 -0600

    Increment DangIt version to Alpha 5

--- source/DangIt/Properties/AssemblyInfo.cs
+++ source/DangIt/Properties/AssemblyInfo.cs
@@ -2,8 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.4.4.0")]
-[assembly: AssemblyFileVersion("0.4.4.0")]
+[assembly: AssemblyVersion("0.5.*")]
 
 [assembly: AssemblyTitle("DangIt")]
 [assembly: AssemblyDescription("Random failures mod for KSP")]
