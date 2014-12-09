7114600b2568295659bb7f1426f7db96123b7c79
Louis Goessling <louis@goessling.com>
2014-12-09 10:20:41 -0600

    Added a KSPAssembly tag

--- source/DangIt/Properties/AssemblyInfo.cs
+++ source/DangIt/Properties/AssemblyInfo.cs
@@ -21,3 +21,5 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("91a53ca5-e3e3-41c1-a837-a125b9a2dff2")]
+
+[assembly: KSPAssembly("DangIt", 0, 5)] //I've called it this because it is Alpha 5. @ippo is this OK?
